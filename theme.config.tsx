a6a211b070d4b5d1f04a89ad29518c4beee50eba
chagchagchag <chagchagchag.dev@gmail.com>
2024-05-19 20:11:17 +0900

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-react-nextjs-study-log',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-react-nextjs-study-log',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
